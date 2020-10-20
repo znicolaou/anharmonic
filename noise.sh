45282d1edbc2f61bd5f24f7080771df74828df56
znicolaou <zack.nicolaou@gmail.com>
2020-10-19 22:04:05 -0700

    updated norm

--- noise.sh
+++ noise.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -n 16
-#SBATCH -a [0-10]
-#SBATCH --mem=16gb
+#SBATCH -a [1-11]
+#SBATCH --mem=64gb
 #SBATCH --output=outs/%j.out
 #SBATCH -p GTX980
 
@@ -9,26 +9,34 @@ module load python/anaconda3.7
 source activate my_env
 export OMP_NUM_THREADS=1
 
-jid=$((SLURM_ARRAY_TASK_ID))
+jid=$((SLURM_ARRAY_TASK_ID-1))
 ZGN_noise=`bc -l <<< "0.0+(0.04-0.0)/10*$jid"`
 
 #calculate high amplitudes initial states
 filebase=data/noise/${jid}_1
-./pendula.py --verbose 0 --num 1000 --cycles 10000 --outcycle 5000 --dt 0.5 --amplitude 0.05 --noise $ZGN_noise --filebase $filebase &
+if [ ! -f ${filebase}fs.npy ]; then
+./pendula.py --verbose 0 --num 100 --initcycle 0 --cycles 50000 --outcycle 40000 --dt 0.5 --initamplitude 0.05 --amplitude 0.05 --noise $ZGN_noise --noisestep 10 --filebase $filebase &
+else
+echo "previously completed"
+fi
 
 #increase from 0.04
 for tid in `seq 0 50`; do
+
 ZGN_amplitude=`bc -l <<< "0.04+0.01/50*$tid"`
 filebase=data/noise/${jid}_${tid}_0
 echo $jid $tid $filebase
-./pendula.py --verbose 0 --num 1000 --initcycle 2000 --cycles 5000 --outcycle 2500 --dt 0.05 --initamplitude 0.04 --amplitude $ZGN_amplitude --noise $ZGN_noise --filebase $filebase &
+if [ ! -f ${filebase}fs.npy ]; then
+./pendula.py --verbose 0 --num 100 --initcycle 10000 --cycles 50000 --outcycle 40000 --dt 0.5 --initamplitude 0.04 --amplitude $ZGN_amplitude --noise $ZGN_noise --noisestep 10 --filebase $filebase &
+else
+echo "previously completed"
+fi
 js=`jobs | wc -l`
 while [ $js -ge 16 ]; do
 sleep 1
 js=`jobs | wc -l`;
 done
 done
-wait
 
 #decrease from 0.05
 for tid in `seq 0 50`; do
@@ -36,7 +44,11 @@ ZGN_amplitude=`bc -l <<< "0.04+0.01/50*$tid"`
 filebase=data/noise/${jid}_${tid}_1
 cp data/noise/${jid}_1fs.npy data/noise/${jid}_${tid}_1ic.npy
 echo $jid $tid $filebase
-./pendula.py --verbose 0 --num 1000 --initcycle 2000 --cycles 5000 --outcycle 2500 --dt 0.05 --initamplitude 0.05 --amplitude $ZGN_amplitude --noise $ZGN_noise --filebase $filebase &
+if [ ! -f ${filebase}fs.npy ]; then
+./pendula.py --verbose 0 --num 100 --initcycle 10000 --cycles 50000 --outcycle 40000 --dt 0.5 --initamplitude 0.05 --amplitude $ZGN_amplitude --noise $ZGN_noise --noisestep 10 --filebase $filebase &
+else
+echo "previously completed"
+fi
 js=`jobs | wc -l`
 while [ $js -ge 16 ]; do
 sleep 1
