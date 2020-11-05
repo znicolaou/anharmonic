3063c3a4539769be6b0bb34084583747635d1e25
znicolaou <zack.nicolaou@gmail.com>
2020-11-05 12:22:40 -0800

    updated script

--- critical.sh
+++ critical.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -n 8
-#SBATCH -a [1-31]
+#SBATCH -a [1-30]
 #SBATCH --mem=64gb
 #SBATCH --output=outs/%j.out
 #SBATCH -p GTX980
@@ -17,11 +17,11 @@ ZGN_freq1=3.75
 ZGN_amp0=0.03
 ZGN_amp1=0.05
 ZGN_num=50
-ZGN_freq=`bc -l <<< "${ZGN_freq0}+(${ZGN_freq1}-${ZGN_freq0})/30*$jid"`
+ZGN_freq=`bc -l <<< "${ZGN_freq0}+(${ZGN_freq1}-${ZGN_freq0})/29*$jid"`
 ZGN_noise=0
-ZGN_cycle0=1000
-ZGN_cycles=5000
-ZGN_cycle1=4000
+ZGN_cycle0=5000
+ZGN_cycles=10000
+ZGN_cycle1=6000
 
 #calculate ZGN_amp1 initial states
 filebase=${ZGN_filebase0}/${jid}ic
@@ -33,7 +33,7 @@ fi
 
 #increase from ZGN_amp0
 for tid in `seq 0 $ZGN_num`; do
-ZGN_amplitude=`bc -l <<< "${ZGN_amp0}+${ZGN_amp1-$ZGN_amp0}/${ZGN_num}*$tid"`
+ZGN_amplitude=`bc -l <<< "${ZGN_amp0}+(${ZGN_amp1}-${ZGN_amp0})/${ZGN_num}*$tid"`
 filebase=${ZGN_filebase0}/${jid}_${tid}_0
 echo $jid $tid $filebase
 if [ ! -f ${filebase}fs.npy ]; then
@@ -50,7 +50,7 @@ done
 
 #decrease from ZGN_amp1
 for tid in `seq 0 $ZGN_num`; do
-ZGN_amplitude=`bc -l <<< "${ZGN_amp0}+${ZGN_amp1-$ZGN_amp0}/${ZGN_num}*$tid"`
+ZGN_amplitude=`bc -l <<< "${ZGN_amp0}+(${ZGN_amp1}-${ZGN_amp0})/${ZGN_num}*$tid"`
 filebase=${ZGN_filebase0}/${jid}_${tid}_1
 cp ${ZGN_filebase0}/${jid}icfs.npy ${ZGN_filebase0}/${jid}_${tid}_1ic.npy
 echo $jid $tid $filebase
