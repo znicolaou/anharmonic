96a3825d21d18d5f55bb8668330e68c2699944ea
znicolaou <zack.nicolaou@gmail.com>
2020-11-05 16:02:04 -0800

    updated script

--- critical.sh
+++ critical.sh
@@ -15,14 +15,15 @@ mkdir -p $ZGN_filebase0
 ZGN_freq0=2.0
 ZGN_freq1=3.75
 ZGN_amp0=0.035
-ZGN_amp1=0.045
+ZGN_amp1=0.0475
 ZGN_num=50
 ZGN_freq=`bc -l <<< "${ZGN_freq0}+(${ZGN_freq1}-${ZGN_freq0})/29*$jid"`
-ZGN_noise=1e-6
+ZGN_noise=0
 ZGN_cycle0=10000
 ZGN_cycles=50000
 ZGN_cycle1=20000
 
+#Maybe we should use modal initial conditions with no defects...
 #calculate ZGN_amp1 initial states
 filebase=${ZGN_filebase0}/${jid}_ic
 if [ ! -f ${filebase}fs.npy ]; then
@@ -37,7 +38,7 @@ ZGN_amplitude=`bc -l <<< "${ZGN_amp0}+(${ZGN_amp1}-${ZGN_amp0})/${ZGN_num}*$tid"
 filebase=${ZGN_filebase0}/${jid}_${tid}_0
 echo $jid $tid $filebase
 if [ ! -f ${filebase}fs.npy ]; then
-./pendula.py --verbose 0 --num 100 --initcycle $ZGN_cycle0 --cycles $ZGN_cycles --outcycle $ZGN_cycle1 --dt 0.5 --initamplitude $ZGN_amp0 --amplitude $ZGN_amplitude --frequency $ZGN_freq --noise $ZGN_noise --noisestep 10 --filebase $filebase &
+./pendula.py --verbose 0 --num 100 --initcycle 0 --cycles $ZGN_cycles --outcycle $ZGN_cycle1 --dt 0.5 --initamplitude $ZGN_amplitude --amplitude $ZGN_amplitude --frequency $ZGN_freq --noise $ZGN_noise --noisestep 10 --filebase $filebase &
 else
 echo "previously completed"
 fi
