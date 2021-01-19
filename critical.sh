1d3812727a8d3eaca4a3d0ed4b94a1682ebe37ab
Zack Nicolaou <zack.nicolaou@gmail.com>
2021-01-19 15:47:42 -0800

    updated critical

--- critical.sh
+++ critical.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH -n 16
-#SBATCH -a [1-64]
+#SBATCH -n 8
+#SBATCH -a [1-32]
 #SBATCH --mem=64gb
 #SBATCH --output=outs/%j.out
 #SBATCH -p GTX980
@@ -17,7 +17,7 @@ ZGN_freq0=3.3
 ZGN_freq1=3.7
 ZGN_amp0=0.02
 ZGN_amp1=0.05
-ZGN_steps=64
+ZGN_steps=32
 ZGN_cycles=5000
 ZGN_outcycle=4000
 ZGN_initcycle=1000
@@ -35,7 +35,7 @@ else
 echo "previously completed"
 fi
 
-#increase from 0.025
+#increase from ZGN_amp0
 for tid in `seq 0 $ZGN_steps`; do
 ZGN_amp=`bc -l <<< "${ZGN_amp0}+(${ZGN_amp1}-${ZGN_amp0})/${ZGN_steps}*$jid"`
 filebase=${filebase0}/${jid}_${tid}_0
@@ -46,18 +46,18 @@ else
 echo "previously completed"
 fi
 js=`jobs | wc -l`
-while [ $js -ge 16 ]; do
+while [ $js -ge 8 ]; do
 sleep 1
 js=`jobs | wc -l`;
 done
 done
 wait
 
-#decrease from 0.05
+#decrease from ZGN_amp1
 for tid in `seq 0 $ZGN_steps`; do
 ZGN_amp=`bc -l <<< "${ZGN_amp0}+(${ZGN_amp1}-${ZGN_amp0})/${ZGN_steps}*$jid"`
 filebase=${filebase0}/${jid}_${tid}_1
-mv ${filebase0}/${jid}_1fs.npy ${filebase0}/${jid}_${tid}_1ic.npy
+cp ${filebase0}/${jid}_1fs.npy ${filebase0}/${jid}_${tid}_1ic.npy
 echo $jid $tid $filebase
 if [ ! -f ${filebase}fs.npy ]; then
 ./pendula.py --verbose 0 --delta $ZGN_delta --num $ZGN_num --initcycle $ZGN_initcycle --cycles $ZGN_cycles --outcycle $ZGN_outcycle --dt 0.5 --initamplitude $ZGN_amp1 --amplitude $ZGN_amp --frequency $ZGN_freq --noise $ZGN_noise --noisestep 10 --filebase $filebase &
@@ -65,7 +65,7 @@ else
 echo "previously completed"
 fi
 js=`jobs | wc -l`
-while [ $js -ge 16 ]; do
+while [ $js -ge 8 ]; do
 sleep 1
 js=`jobs | wc -l`;
 done
