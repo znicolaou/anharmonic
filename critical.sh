a1c9d39fbd5fd17c5d6b04592e0e71100f28be5f
Zack Nicolaou <zack.nicolaou@gmail.com>
2021-01-19 20:59:40 -0800

    updated critical

--- critical.sh
+++ critical.sh
@@ -32,7 +32,7 @@ for tid in `seq 0 $ZGN_steps`; do
 ZGN_amp=`bc -l <<< "${ZGN_amp0}+(${ZGN_amp1}-${ZGN_amp0})/${ZGN_steps}*$tid"`
 filebase=${filebase0}/${jid}_${tid}_0
 echo $jid $tid $filebase
-if [ ! -f ${filebase}out.dat ]; then
+if [ ! -f ${filebase}fs.npy ]; then
 ./pendula.py --verbose 0 --delta $ZGN_delta --num $ZGN_num --initcycle 0 --cycles $ZGN_cycles --outcycle $ZGN_outcycle --dt 0.5 --initamplitude $ZGN_amp --amplitude $ZGN_amp --initfrequency $ZGN_freq --frequency $ZGN_freq --noise $ZGN_noise --noisestep 10 --filebase $filebase &
 else
 echo "previously completed"
@@ -62,7 +62,7 @@ ZGN_amp=`bc -l <<< "${ZGN_amp0}+(${ZGN_amp1}-${ZGN_amp0})/${ZGN_steps}*$tid"`
 filebase=${filebase0}/${jid}_${tid}_1
 cp ${filebase1}_0fs.npy ${filebase0}/${jid}_${tid}_1ic.npy
 echo $jid $tid $filebase
-if [ ! -f ${filebase}out.dat ]; then
+if [ ! -f ${filebase}fs.npy ]; then
 ./pendula.py --verbose 0 --delta $ZGN_delta --num $ZGN_num --initcycle $ZGN_initcycle --cycles $ZGN_cycles --outcycle $ZGN_outcycle --dt 0.5 --initamplitude $ZGN_amp1 --amplitude $ZGN_amp --initfrequency $ZGN_freq --frequency $ZGN_freq --noise $ZGN_noise --noisestep 10 --filebase $filebase &
 else
 echo "previously completed"
@@ -97,7 +97,7 @@ ZGN_freq=`bc -l <<< "${ZGN_freq0}+(${ZGN_freq1}-${ZGN_freq0})/${ZGN_steps}*$jid2
 filebase=${filebase0}/${jid}_${tid}_2_${jid2}
 cp ${filebase1}_0fs.npy ${filebase}ic.npy
 echo $jid $tid $filebase
-if [ ! -f ${filebase}out.dat ]; then
+if [ ! -f ${filebase}fs.npy ]; then
 ./pendula.py --verbose 0 --delta $ZGN_delta --num $ZGN_num --initcycle $ZGN_initcycle --cycles $ZGN_cycles --outcycle $ZGN_outcycle --dt 0.5 --initamplitude $ZGN_amp --amplitude $ZGN_amp --initfrequency $ZGN_freq0 --frequency $ZGN_freq --noise $ZGN_noise --noisestep 10 --filebase $filebase &
 else
 echo "previously completed"
